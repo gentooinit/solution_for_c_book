b5eb1dda9ff8ba0b857292f2b90383b4147d547b
init0 <gentooinit@hotmail.com>
2015-12-02 17:29:00 +0800

    Add exercise1-1

---
+++ Chapter1/hello.c
@@ -0,0 +1,19 @@
+/*
+ * File name: hello.c
+ *
+ * A solution to K&R The C Programming Language:
+ *
+ * Page 8
+ * Exercise 1-1. Run the "hello, world" program on your system. Experiment
+ * with leaving out parts of the program, to see what error messages you get.
+ *
+ */
+#include <stdio.h>
+
+int main()
+{
+	printf("hello, world\n");
+
+	return 0;
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+init0's solutions for exercises in the K&R's C book
+===
+
+In order to review my C programming language skill, 
+i decided to do all the exercises in the "The C Programming
+Language, 2nd Edition"
+
+* OS: Slackware-current(>=14.1)
+* Compiler: gcc 4.9.3
