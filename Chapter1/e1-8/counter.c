8f4cd9bd8d5a17a2baacd7535ff687e1ddffbd0e
init0 <gentooinit@hotmail.com>
2015-12-25 18:05:07 +0800

    Add exercise1-8

---
+++ Chapter1/e1-8/counter.c
@@ -0,0 +1,39 @@
+/*
+ * File name: counter.c
+ *
+ * A solution to K&R The C Programming Language:
+ *
+ * Page 20
+ * Exercise 1-8. Write a program to count blanks,tabs,and newlines.
+ *
+ */
+/* EOF defined in <stdio.h> */
+#include <stdio.h>
+
+int main()
+{
+	int c;
+	int blank_cnt;
+	int tab_cnt;
+	int newline_cnt;
+
+	blank_cnt = 0;
+	tab_cnt = 0;
+	newline_cnt = 0;
+	while ((c = getchar()) != EOF) {
+		if (c == ' ')
+			++blank_cnt;
+		
+		if (c == '\t')
+			++tab_cnt;
+
+		if (c == '\n')
+			++newline_cnt;
+	}
+
+	printf("blanks: %d, tabs: %d, newlines: %d\n",
+		blank_cnt, tab_cnt, newline_cnt);
+
+	return 0;
+}
+
