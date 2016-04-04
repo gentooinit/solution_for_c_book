ef505570d28596eca9ce4b14280c4aad3fb5409a
init0 <gentooinit@hotmail.com>
2016-04-05 00:01:27 +0800

    e2-1: compute the ranges of float and double

--- Chapter2/e2-1/ranges.c
+++ Chapter2/e2-1/ranges.c
@@ -11,6 +11,7 @@
  *
  */
 #include <stdio.h>
+#include <stdint.h>
 #include <limits.h>
 #include <float.h>
 
@@ -19,8 +20,23 @@
 #define MAX_SIGNED(x) ((unsigned x)~0 >> 1)
 #define MIN_SIGNED(x) (MAX_SIGNED(x) + 1)
 
+#define MAX_NORMALIZED_FLT(exp, frac) (\
+	(0lu << ((exp) + (frac))) |                       /* positive sign*/ \
+	((((1lu << (exp)) - 1) & ~1lu) << (frac)) |       /* max exp */ \
+	((1lu << (frac)) - 1)                             /* max frac */ \
+)
+
+#define MIN_NORMALIZED_FLT(exp, frac) (\
+	(0lu << ((exp) + (frac))) |                       /* positive sign */ \
+	(1lu << (frac))  |                                /* min exp */ \
+	0lu                                               /* min frac */ \
+)
+
 int main()
 {
+	float min_flt, max_flt;
+	double min_dp, max_dp;
+
 	printf("signed char: %hhd~%hhd\n", MIN_SIGNED(char), MAX_SIGNED(char));
 	printf("unsigned char: %hhu~%hhu\n", MIN_UNSIGNED(unsigned char), MAX_UNSIGNED(unsigned char));
 	printf("signed short: %hd~%hd\n", MIN_SIGNED(short), MAX_SIGNED(short));
@@ -29,6 +45,21 @@ int main()
 	printf("unsigned int: %u~%u\n", MIN_UNSIGNED(unsigned int), MAX_UNSIGNED(unsigned int));
 	printf("signed long: %ld~%ld\n", MIN_SIGNED(long), MAX_SIGNED(long));
 	printf("unsigned long: %lu~%lu\n", MIN_UNSIGNED(unsigned long), MAX_UNSIGNED(unsigned long));
+
+	/* min normalized float */
+	*(uint32_t *)(&min_flt)  = MIN_NORMALIZED_FLT(8, 23);
+
+	/* max normalized float */
+	*(uint32_t *)(&max_flt)  = MAX_NORMALIZED_FLT(8, 23);
+
+	/* min normalized double */
+	*(uint64_t *)(&min_dp)  = MIN_NORMALIZED_FLT(11, 52);
+
+	/* max normalized double */
+	*(uint64_t *)(&max_dp)  = MAX_NORMALIZED_FLT(11, 52);
+
+	printf("float: %g~%g\n", min_flt, max_flt);
+	printf("double: %g~%g\n", min_dp, max_dp);
 
 	
 	/* from header */
@@ -44,6 +75,7 @@ int main()
 
 	printf("float: %g~%g\n", FLT_MIN, FLT_MAX);
 	printf("double: %g~%g\n", DBL_MIN, DBL_MAX);
+
 	return 0;
 }
 
